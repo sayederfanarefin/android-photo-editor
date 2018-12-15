d223579537952305f202a82011c5be83683c85ba
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-12-15 16:06:05 +0100

    init

---
+++ app/src/main/java/info/sayederfanarefin/imageeditor/filters/FilterListener.java
@@ -0,0 +1,7 @@
+package info.sayederfanarefin.imageeditor.filters;
+
+import ja.burhanrashid52.photoeditor.PhotoFilter;
+
+public interface FilterListener {
+    void onFilterSelected(PhotoFilter photoFilter);
+}
